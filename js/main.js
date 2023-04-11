6a44e0461d3c55b9ba25b0100c8d26e0e70b8f6e
Fayzullin Zagir <105968987+Zzzoga@users.noreply.github.com>
2023-04-11 09:42:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -79,7 +79,45 @@ function getCookie(name) {
   } else {
     document.querySelector('.preloader').style.display = 'none'
   }
-  
+
+  function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		console.log(template);
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
+	
+  }  
 document.addEventListener("DOMContentLoaded", () => {  
 // Document functions
 
@@ -157,9 +195,12 @@ if (document.documentElement.clientWidth <= 768) {
     })
 }
 
-
+// TwentyTwenty plugin
 $("#container1").twentytwenty();
 $("#container2").twentytwenty();
+
+// PHONE MASK
+maskPhone('.form__input')
 
 // Smooth scroll when link clicked
 const $page = $('html, body');
